497b39e784f007d307eb7be76c31a999ff01cd78
lightmes_depp <yanbin7019@outlook.com>
2021-03-26 09:19:07 +0800

    DeleteDuplicates

---
+++ src/remove_duplicates/DeleteDuplicates2.java
@@ -0,0 +1,19 @@
+package remove_duplicates;
+
+/**
+ * 存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除所有重复的元素，使每个元素 只出现一次 。
+ *
+ * 返回同样按升序排列的结果链表。
+ */
+public class DeleteDuplicates2 {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode temp = head;
+        while (temp != null && temp.next != null) {
+            while (temp.next != null && temp.val == temp.next.val) {
+                temp.next = temp.next.next;
+            }
+            temp = temp.next;
+        }
+        return head;
+    }
+}
