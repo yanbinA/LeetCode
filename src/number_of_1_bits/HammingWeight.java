9619d9f115f98359629bef9db9e6c79d0b05cadc
lightmes_depp <yanbin7019@outlook.com>
2021-03-29 09:42:40 +0800

    .

--- src/number_of_1_bits/HammingWeight.java
+++ src/number_of_1_bits/HammingWeight.java
@@ -1,5 +1,7 @@
 package number_of_1_bits;
 
+import sun.security.util.BitArray;
+
 /**
  * 编写一个函数，输入是一个无符号整数（以二进制串的形式），返回其二进制表达式中数字位数为 '1' 的个数（也被称为汉明重量）。
  */
@@ -16,5 +18,10 @@ public class HammingWeight {
     public static void main(String[] args) {
         HammingWeight weight = new HammingWeight();
         System.out.println(weight.hammingWeight(-3));
+    }
+
+    //颠倒给定的 32 位无符号整数的二进制位。
+    public int reverseBits(int n) {
+        return Integer.reverse(n);
     }
 }
