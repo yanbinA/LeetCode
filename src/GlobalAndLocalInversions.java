960afff49ee6e763607a0dbb638628e622543680
yanbinA <yanbin7019@outlook.com>
2022-11-16 10:36:47 +0800

    GlobalAndLocalInversions

---
+++ src/GlobalAndLocalInversions.java
@@ -0,0 +1,26 @@
+/**
+ * 775. 全局倒置与局部倒置
+ * 给你一个长度为 n 的整数数组 nums ，表示由范围 [0, n - 1] 内所有整数组成的一个排列。
+ *
+ * 全局倒置 的数目等于满足下述条件不同下标对 (i, j) 的数目：
+ *
+ * 0 <= i < j < n
+ * nums[i] > nums[j]
+ * 局部倒置 的数目等于满足下述条件的下标 i 的数目：
+ *
+ * 0 <= i < n - 1
+ * nums[i] > nums[i + 1]
+ * 当数组 nums 中 全局倒置 的数量等于 局部倒置 的数量时，返回 true ；否则，返回 false 。
+ */
+public class GlobalAndLocalInversions {
+    public boolean isIdealPermutation(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            int num = nums[i];
+            if (num - i > 1 || num - i < -1) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
